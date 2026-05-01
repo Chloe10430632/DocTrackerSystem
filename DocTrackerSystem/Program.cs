b30ab6d9217db45af7dd5cff7d386e3e40a233b3
Chloe10430632 <a21307096@gmail.com>
2026-05-02 02:51:31 +0800

    完成登入與閱讀頁

--- DocTrackerSystem/Program.cs
+++ DocTrackerSystem/Program.cs
@@ -116,6 +116,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Login}/{id?}");
 
 app.Run();
